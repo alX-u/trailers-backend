c6e742260bd87d25c7eff9bf7e3209f976adb962
alX-u <team4@overnights.com>
2025-05-22 09:31:56 -0500

    cors policies

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
