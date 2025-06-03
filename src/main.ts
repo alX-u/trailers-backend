69c413865ca945d62ee4f75fdb6392cc3d074372
alX-u <team4@overnights.com>
2025-06-03 10:52:14 -0500

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://trailersdelcaribe.net'],
     credentials: true,
   });
 
